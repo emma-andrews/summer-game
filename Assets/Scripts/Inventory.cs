de5de13d07b856fabae90d017569a83cb44915a0
Emma <eandrews258@gmail.com>
2018-05-22 22:33:15 -0400

    added inventory ui

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour {
+
+    public const int numItemSlots = 4;
+    public Image[] itemImages = new Image[numItemSlots];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
